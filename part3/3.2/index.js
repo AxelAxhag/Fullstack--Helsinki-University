b72d18dfa8ed2a2ac1ca28bb059e71791a9b4bd8
Axel Axhag <thesniperwiper@gmail.com>
2025-07-14 22:05:51 +0200

    3.2

---
+++ part3/3.2/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+
+const persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send("<h1>Root page</h1>")
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const amount_contacts = persons.length
+    const people_or_person = amount_contacts === 1 ? "person" : "people"
+    const page = `<div>The phonebook contains ${amount_contacts} ${people_or_person}</div>
+                  <div>${new Date().toString()}</div>`
+    
+    response.send(page)
+})
+
+const PORT = 3001 
+app.listen(PORT)
