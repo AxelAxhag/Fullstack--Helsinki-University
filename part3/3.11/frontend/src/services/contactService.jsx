79c8faa642cb4463010584ed2705de498f1a8d15
Axel Axhag <thesniperwiper@gmail.com>
2025-07-21 15:38:11 +0200

    part3.11 (frontend+backend)

---
+++ part3/3.11/frontend/src/services/contactService.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+const url = "/api/persons"
+
+const getAll = () => {
+    return axios
+        .get(url)
+        .then(response => response.data)
+}
+
+const create = (newContact) => {
+    return axios
+        .post(url, newContact)
+        .then(response => response.data)
+}
+
+const update = (id, newContact) => {
+    return axios
+        .put(`${url}/${id}`, newContact)
+        .then(response => response.data)
+}
+
+const deleteContact = (id) => {
+    return axios
+        .delete(`${url}/${id}`)
+        .then(response => response.data)
+}
+
+export default {
+    getAll,
+    create,
+    update,
+    deleteContact
+}
